02d59436e42f6f795f0412404d39c41f5af5b8e0
ardana06 <fhggffhf@gmail.com>
2025-06-11 02:09:33 +0500

    add main dekstop

--- project-anime/src/App.jsx
+++ project-anime/src/App.jsx
@@ -4,3 +4,38 @@ import AnimeCard from "./AnimeCard";
 import Modal from "./Modal";
 import { useTheme } from "./ThemeContext";
 import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [genre, setGenre] = useState("");
+  const [page, setPage] = useState(1);
+  const [selectedAnime, setSelectedAnime] = useState(null);
+
+  const {
+    data: animeList,
+    loading,
+    error,
+    genres,
+  } = useFetchAnime(query, genre, page);
+  const { theme, toggleTheme } = useTheme();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setPage(1);
+    setQuery(e.target.elements.search.value.trim());
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+    setPage(1);
+  };
+
+  const handleRandom = async () => {
+    try {
+      const res = await fetch("https://api.jikan.moe/v4/random/anime");
+      const json = await res.json();
+      setSelectedAnime(json.data);
+    } catch {
+      alert("Failed to fetch random anime");
+    }
+  };
